c8fd2f4bf3b82027e80c7fc1d7c3fed20bd0bd08
Dimitar Kovachev <d.kovachev@gmail.com>
2012-06-23 23:02:07 +0300

    Added delays and T0 sampling

--- AVR_Electrolytic_C_ESR_R_Meter/AVR_Electrolytic_C_ESR_R_Meter.ino
+++ AVR_Electrolytic_C_ESR_R_Meter/AVR_Electrolytic_C_ESR_R_Meter.ino
@@ -11,8 +11,8 @@ const int chargePin = 6;                      // The constant-current source is 
 const int dischargePin = 7;                   // The discharging MOSFET is controlled by this pin. LOW is not-discharging, HIGH is discharging
 const int voltagePin = A0;                    // The capacitor voltage is sampled on this pin
 
-const double chargeCurrentMA = 1.285;           // The measured current, provided by the constant current source. Expressed in milli-Amperes.
-const double internalReferenceVoltage = 1.081;  // Arduino's internal ADC reference voltage, as measured at the AREF pin
+const double chargeCurrentMA = 52.6;           // The measured current, provided by the constant current source. Expressed in milli-Amperes.
+const double internalReferenceVoltage = 1.1;  // Arduino's internal ADC reference voltage
 
 const double targetCapacitorVoltage = 0.2;      // The capacitor will be charged up to this voltage. Expressed in Volts.
 const double openCircuitThresholdVoltage = 0.4; // A voltages above this threshold indicates that a capacitor is NOT present at the test terminals. Expressed in Volts.
@@ -56,26 +56,34 @@ void setup()
 
 void loop()
 {
+  measureESR();
+  discharge();  
+  delay(1000);
+
   chargeAndMeasure();
   discharge();  
+  Serial.print("\n");
   delay(5000);
 }
 
 
 void chargeAndMeasure()
 {
+  digitalWrite(dischargePin, LOW);  // Stop discharging
+  delay(10);
+
   // Note the moment the charging starts
   unsigned long startTime = micros();
-  
-  digitalWrite(dischargePin, LOW);  // Stop discharging
   digitalWrite(chargePin, LOW);     // Start charging
+ 
   delayMicroseconds(1);
   int esrReading = analogRead(voltagePin);
- 
+  
   int currentReading = 0; 
   unsigned long elapsedTimeUS = 0;
   while (true)
   {
+    elapsedTimeUS = micros() - startTime;
     currentReading = analogRead(voltagePin);
     
     // Check if a capacitor is present
@@ -85,19 +93,19 @@ void chargeAndMeasure()
       return;
     }
     
-    elapsedTimeUS = micros() - startTime;
     if (currentReading >= targetVoltageReading || elapsedTimeUS >= chargeTimeoutUS)
     {
+      digitalWrite(chargePin, HIGH);     // Stop charging
+
       // Estimate capacity: C = (i * t) / v
       double voltageMV = readingToVoltage(currentReading) * 1000.0;  // In milli-Volts
       double c = (chargeCurrentMA * elapsedTimeUS) / voltageMV;
 
-      Serial.print("Charged in ");
-      Serial.print(elapsedTimeUS);
-      Serial.print(" usec\n");
       Serial.print("Voltage=");
       Serial.print(voltageMV);
-      Serial.print(" mV\n");
+      Serial.print(" mV  @ ");
+      Serial.print(elapsedTimeUS);
+      Serial.print(" usec\n");
       Serial.print("C=");
       Serial.print(c);
       Serial.print(" uF\n");
@@ -105,7 +113,7 @@ void chargeAndMeasure()
       double esrVoltageMV = readingToVoltage(esrReading) * 1000.0;  // In milli-Volts
       double esr = esrVoltageMV / chargeCurrentMA;    // In Ohms
 
-      Serial.print("V0=");
+      Serial.print("V_ESR=");
       Serial.print(esrVoltageMV);
       Serial.print(" mV\n");
       Serial.print("ESR=");
@@ -115,10 +123,49 @@ void chargeAndMeasure()
       Serial.print("V'=");
       Serial.print(voltageMV - esrVoltageMV);
       Serial.print(" mV\n");
-      Serial.print("C=");
+      Serial.print("C'=");
       Serial.print((chargeCurrentMA * elapsedTimeUS)/(voltageMV - esrVoltageMV));
       Serial.print(" uF\n");      
+      
       return;
+    }
+  }
+}
+
+void measureESR()
+{
+  digitalWrite(dischargePin, LOW);  // Stop discharging
+  delay(10);
+
+  // Note the moment the charging starts
+  unsigned long startTime = micros();
+  digitalWrite(chargePin, LOW);     // Start charging
+  delayMicroseconds(1);
+  
+  int readings[10]; 
+  unsigned long elapsedTimeUS = 0;
+  
+  for (int i = 0; i < 10; ++i)
+  {
+    readings[i] = analogRead(voltagePin);
+  }
+  unsigned long endTime = micros();
+
+  // Report
+  if (readings[0] < openCircuitVoltageReading)
+  {
+    Serial.print("10 ADC readings in ");
+    Serial.print(endTime - startTime);
+    Serial.print(" uS\n");
+
+    for (int i = 0; i < 10; ++i)
+    {
+        Serial.print("Voltage[");
+        Serial.print(i);
+        Serial.print("] = ");
+        Serial.print(readingToVoltage(readings[i]) * 1000.0);
+        Serial.print(" mV");
+        Serial.print("\n");
     }
   }
 }
@@ -130,6 +177,8 @@ void discharge()
 
   digitalWrite(chargePin, HIGH);     // Stop charging
   digitalWrite(dischargePin, HIGH);  // Start discharging
+  
+  delay(5);
 
   while (analogRead(voltagePin) > 0)
   {
@@ -153,3 +202,4 @@ inline static double readingToVoltage(int reading)
 {
   return reading * (internalReferenceVoltage / 1024.0);
 }
+
